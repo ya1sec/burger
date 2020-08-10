c36b4133a378f39ce6caa6c0699245f275c55265
unknown <theankitbagchi@gmail.com>
2020-08-10 16:21:40 -0400

    Process.env

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 // Set up MySQL connection.
+require("dotenv").config();
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "boolean10",
-  database: "burgers_db",
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env, JAWSDB_URL);
+} else {
+  connection = mysql.createConnection(process.env.DB_URL);
+}
 
 // Make connection.
 connection.connect(function (err) {
