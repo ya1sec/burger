dec4b998d5a21c0deff6799c451dc95f2d864767
Ankit Bagchi <theankitbagchi@gmail.com>
2020-08-09 22:24:22 -0400

    burgers added to table

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -31,37 +31,20 @@ router.put("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
-  if (req.body.devoured === 0) {
-    burger.update(
-      {
-        devoured: true,
-      },
-      condition,
-      function (result) {
-        if (result.changedRows == 0) {
-          // If no rows were changed, then the ID must not exist, so 404
-          return res.status(404).end();
-        } else {
-          res.status(200).end();
-        }
+  burger.update(
+    {
+      devoured: true,
+    },
+    condition,
+    function (result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
       }
-    );
-  } else {
-    burger.update(
-      {
-        devoured: false,
-      },
-      condition,
-      function (result) {
-        if (result.changedRows == 0) {
-          // If no rows were changed, then the ID must not exist, so 404
-          return res.status(404).end();
-        } else {
-          res.status(200).end();
-        }
-      }
-    );
-  }
+    }
+  );
 });
 
 router.delete("/api/burgers/:id", function (req, res) {
